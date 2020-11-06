fb868d2f142f6cf1cc855b9785f00f9fece8ff5d
Emmanuel <ogahemmanuel75@gmail.com>
2020-11-06 18:16:26 +0100

    Update SignupForm.js

--- src/containers/SignupForm/SignupForm.js
+++ src/containers/SignupForm/SignupForm.js
@@ -17,23 +17,31 @@ class SignupForm extends Component {
         "lastname": "string"
     } 
 
-    signupDataHandler = () => {
+    signupDataHandler = (event) => {
+        event.preventDefault();
         const data = {
-            dailingCode: this.state.dailingCode,
-            phoneNumber: this.state.phoneNumber,
-            howDoyouKnowUs: this.state.howDoyouKnowUs,
-            refernceid: this.state.refernceid,
-            countryId: this.state.countryId,
-            emailAddress: this.state.emailAddress,
-            password: this.state.password,
-            confirmPassword: this.state.confirmPassword,
-            firstname: this.state.firstname,
-            lastname: this.state.lastname
+            dailingCode: "",
+            phoneNumber: "",
+            howDoyouKnowUs: "",
+            refernceid: "",
+            countryId: 1,
+            emailAddress: "",
+            password: "",
+            confirmPassword: "",
+            firstname: "",
+            lastname: ""
 
         }
-        axios.post('http://webapi.synergyindustries.org/api​/registration​/user', data)
+        // console.log(data);
+        let url = 'http://webapi.synergyindustries.org/api/registration/user';
+        var header = {
+            "Content-Type": "application/json"
+        }
+        axios.post(url, data, header)
             .then(response => {
                 console.log(response);
+            }).catch((err) => {
+                console.log('An error occured ', err);
             });
     }
 
@@ -42,7 +50,7 @@ class SignupForm extends Component {
         return(
             <div className="SignupForm">
                 <h3>Registerion Form</h3>
-                <form>
+                <form onSubmit={this.signupDataHandler}>
                     <input className="Input" type="text" name="dailingCode" placeholder="Your dailing code"/>
                     <input className="Input" type="text" name="phoneNumber" placeholder="Phone number"/>
                     <input className="Input" type="text" name="howDoyouKnowUs" placeholder="How do you know about us"/>
@@ -53,7 +61,7 @@ class SignupForm extends Component {
                     <input className="Input" type="password" name="confirmPassword" placeholder="Confirm password"/>
                     <input className="Input" type="text" name="firstName" placeholder="First Name"/>
                     <input className="Input" type="text" name="lastName" placeholder="Last Name"/>
-                    <button onClick={this.signupDataHandler}>SignUp</button>
+                    <button >SignUp</button>
                 </form>
             </div>
         );
